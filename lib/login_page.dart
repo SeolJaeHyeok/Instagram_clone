f40de47df446f42076e30ffbad8c9897b46cedb7
JaeHyeok <milkboy2564@Naver.com>
2020-10-07 22:03:01 +0900

    Firebasedhk Google 로그인 기능 구현

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,7 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatelessWidget {
+import 'tab_page.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,11 +28,26 @@ class LoginPage extends StatelessWidget {
             Padding(padding: EdgeInsets.all(50.0)),
             SignInButton(
               Buttons.Google,
-              onPressed: (){},
+              onPressed: (){
+                _handleSignIn().then((user){
+                  Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => TabPage(user)));
+                });
+              },
             )
           ],
         ),
       ),
     );
+  }
+
+  Future _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = (await _auth.signInWithCredential(
+      GoogleAuthProvider.getCredential(
+          idToken: googleAuth.idToken, accessToken: googleAuth.accessToken))).user;
+
+    return user;
   }
 }
